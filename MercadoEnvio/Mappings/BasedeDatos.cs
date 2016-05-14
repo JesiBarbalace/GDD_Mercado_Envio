2c9c5bad6335e38f62cab062d019077048d8f589
JesiBarbalace <jesica.barbalace@gmail.com>
2016-05-14 14:55:01 -0300

    Commit Inicial
    Commit Inicial

---
+++ MercadoEnvio/Mappings/BasedeDatos.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace AerolineaFrba
+{
+    public partial class BasedeDatos : Form
+    {
+        public BasedeDatos()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        public SqlConnection conexion = new SqlConnection("Data Source=JESICAB-PC\\SQLSERVER2012;" + "Initial Catalog=GD1C2016;" + "Integrated Security=true;"
+                + "UID=gd" + "PWD=gd2016");
+
+
+        public void openConnection()
+        {
+            conexion.Open();
+        }
+
+        public void closeConnection() { conexion.Close(); }
+
+        public void query(String query)
+        {
+            try
+            {
+                conexion.Open();
+                SqlCommand queryCommand = new SqlCommand(query, conexion);
+                SqlDataReader queryCommandReader = queryCommand.ExecuteReader();
+            
+                conexion.Close();
+               
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message + " Query: " + query);
+
+            }
+        }
+
+        public DataTable select_query(String query)
+        {
+
+            try
+            {
+                conexion.Open();
+                SqlCommand queryCommand = new SqlCommand(query, conexion);
+                SqlDataReader queryCommandReader = queryCommand.ExecuteReader();
+                DataTable dataTable = new DataTable();
+                dataTable.Load(queryCommandReader);
+                conexion.Close();
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message + " Query: " + query);
+
+            }
+
+            return new DataTable();
+        }
+
+        private void BasedeDatos_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public  string GetConnectionString(string connectionStringName)
+        {
+            Configuration appconfig =
+                ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            ConnectionStringSettings connStringSettings = appconfig.ConnectionStrings.ConnectionStrings[connectionStringName];
+            return connStringSettings.ConnectionString;
+        }
+
+        public  void SaveConnectionString(string connectionStringName, string connectionString)
+        {
+            Configuration appconfig =
+                ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+            appconfig.ConnectionStrings.ConnectionStrings[connectionStringName].ConnectionString = connectionString;
+            appconfig.Save();
+        }
+
+
+    }
+}
---
+++ MercadoEnvio/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using MercadoEnvio.Mappings;
+using System.IO;
+
+namespace MercadoEnvio
+{
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+
+            //La fecha del sistema se debe modificar desde acá-
+            //Properties.Settings.Default.FechaSistema = DateTime.Now;
+            BasedeDatos bd = new BasedeDatos();
+
+            string a = bd.GetConnectionString("conexion");
+            bd.SaveConnectionString("MercadoEnvio.Properties.Settings.GD1C2016ConnectionString", a);
+            bd.SaveConnectionString("MercadoEnvio.Properties.Settings.GD1C2016ConnectionStringJ", a);
+
+            Properties.Settings.Default.FechaSistema = Convert.ToDateTime("2017-01-31");
+            Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
